67205ea7c8f30e9020c27d6ef6bea9d4f7bc0a5d
ScientistOWl <BlackStoneGame22@gmail.com>
2025-07-25 22:09:42 +0300

    article page item list

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -1,22 +1,34 @@
+import PropTypes from 'prop-types';
 import s from './ArticlesList.module.css';
-
 import ArticleItem from '../ArticleItem/ArticleItem';
 
-const ArticlesList = () => {
-  const articlesList = [];
+const ArticlesList = ({ articles = [], onLoadMore }) => {
+  if (!Array.isArray(articles) || articles.length === 0) {
+    return <p className={s.noArticles}>No articles found.</p>;
+  }
+
   return (
-    <div>
-      <ul>
-        {articlesList.map(item => (
-          <li key={item.id}>
+    <div className={s.wrapper}>
+      <ul className={s.list}>
+        {articles.map((item) => (
+          <li key={item.id} className={s.item}>
             <ArticleItem {...item} />
           </li>
         ))}
       </ul>
 
-      <button type='button'>Load more</button>
+      {onLoadMore && (
+        <button type="button" className={s.loadMore} onClick={onLoadMore}>
+          Load more
+        </button>
+      )}
     </div>
   );
+};
+
+ArticlesList.propTypes = {
+  articles: PropTypes.arrayOf(PropTypes.object),
+  onLoadMore: PropTypes.func,
 };
 
 export default ArticlesList;
