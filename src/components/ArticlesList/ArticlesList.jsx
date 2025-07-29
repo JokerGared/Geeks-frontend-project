7bf6a852135b30accc474df393afe6f9968a964a
Ira Efremova <ira.efremova95@gmail.com>
2025-07-29 18:08:08 +0300

    create ArticlesList

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -1,34 +1,26 @@
-import PropTypes from 'prop-types';
 import s from './ArticlesList.module.css';
 import ArticleItem from '../ArticleItem/ArticleItem';
 
 const ArticlesList = ({ articles = [], onLoadMore }) => {
-  if (!Array.isArray(articles) || articles.length === 0) {
-    return <p className={s.noArticles}>No articles found.</p>;
+  if (articles.length === 0) {
+    return <p className={s.noArticles}>No articles yet</p>;
   }
 
   return (
     <div className={s.wrapper}>
       <ul className={s.list}>
         {articles.map((item) => (
-          <li key={item.id} className={s.item}>
+          <li key={item._id}>
             <ArticleItem {...item} />
           </li>
         ))}
       </ul>
 
-      {onLoadMore && (
-        <button type="button" className={s.loadMore} onClick={onLoadMore}>
-          Load more
-        </button>
-      )}
+      <button type="button" className={s.loadMore} onClick={onLoadMore}>
+        Load more
+      </button>
     </div>
   );
-};
-
-ArticlesList.propTypes = {
-  articles: PropTypes.arrayOf(PropTypes.object),
-  onLoadMore: PropTypes.func,
 };
 
 export default ArticlesList;
