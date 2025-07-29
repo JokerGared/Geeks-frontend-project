2f9ed4f1041e38ed4c0a8e852b8c90919611d754
irina8660 <ira.efremova95@gmail.com>
2025-07-29 18:15:41 +0300

    Update ButtonEdit.jsx

--- src/components/ButtonEdit/ButtonEdit.jsx
+++ src/components/ButtonEdit/ButtonEdit.jsx
@@ -1,19 +1,12 @@
 import clsx from 'clsx';
+import s from './ButtonEdit.module.css';
 
-import s from './ButtonAddToBookmarks.module.css';
-
-const ButtonEdit = ({ article }) => {
-  const buttonClass = clsx(s.button, { [s.active]: isSaved });
-
+const ButtonEdit = () => {
   return (
-    <button className={buttonClass} type="button" onClick={handleClick}>
-      {isLoading ? (
-        <Loader size={24} />
-      ) : (
-        <svg className={s.icon} width="24px" height="24px">
-          <use xlinkHref="/icons.svg#icon-edit" />
-        </svg>
-      )}
+    <button className={s.edit} type="button">
+      <svg className={s.icon} width="24px" height="24px">
+        <use xlinkHref="/icons.svg#icon-edit" />
+      </svg>
     </button>
   );
 };
