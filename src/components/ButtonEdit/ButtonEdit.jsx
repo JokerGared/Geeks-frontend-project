d525833e11ffe7e8d2001ab78dc8ea12c1d21985
Ira Efremova <ira.efremova95@gmail.com>
2025-07-29 22:32:40 +0300

    add ButtonEdit

--- src/components/ButtonEdit/ButtonEdit.jsx
+++ src/components/ButtonEdit/ButtonEdit.jsx
@@ -1,9 +1,15 @@
-import clsx from 'clsx';
+import { useNavigate } from 'react-router-dom';
 import s from './ButtonEdit.module.css';
 
-const ButtonEdit = () => {
+const ButtonEdit = ({ article }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/create-article', { state: { article } });
+  };
+
   return (
-    <button className={s.button} type="button">
+    <button className={s.button} type="button" onClick={handleClick}>
       <svg className={s.icon} width="24px" height="24px">
         <use xlinkHref="/icons.svg#icon-edit" />
       </svg>
