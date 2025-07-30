e24da8b39c20628a33903cc2f91aca215c922a49
Ira Efremova <ira.efremova95@gmail.com>
2025-07-31 02:52:58 +0300

    add props  isLoading & hasNextPage

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -1,24 +1,32 @@
 import s from './ArticlesList.module.css';
 import ArticleItem from '../ArticleItem/ArticleItem';
 
-const ArticlesList = ({ articles = [], onLoadMore }) => {
-  if (articles.length === 0) {
-    return <p className={s.noArticles}>No articles yet</p>;
-  }
-
+const ArticlesList = ({
+  articles = [],
+  isLoading,
+  hasNextPage,
+  onLoadMore,
+}) => {
   return (
     <div className={s.wrapper}>
       <ul className={s.list}>
-        {articles.map((item) => (
-          <li key={item._id}>
-            <ArticleItem {...item} />
+        {articles.map((article) => (
+          <li key={article._id}>
+            <ArticleItem {...article} />
           </li>
         ))}
       </ul>
 
-      <button type="button" className={s.loadMore} onClick={onLoadMore}>
-        Load more
-      </button>
+      {hasNextPage && (
+        <button
+          type="button"
+          className={s.loadMore}
+          onClick={onLoadMore}
+          disabled={isLoading}
+        >
+          {isLoading ? 'Loading...' : 'Load more'}
+        </button>
+      )}
     </div>
   );
 };
