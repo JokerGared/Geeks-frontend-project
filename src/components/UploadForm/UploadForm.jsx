ac7a642dcc5f7a91758ffaab5374e40b0a57cea3
Yurii <Jahnavart@gmail.com>
2025-07-30 19:26:35 +0300

    fix 2

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -29,7 +29,7 @@ const UploadForm = () => {
       // dispatch(setPhotoData(selectedImage));
       console.log('Фото загружено:', selectedImage);
     }
-    navigate('/HomeAuthorised');
+    navigate('/');
   };
 
   const handleClose = () => {
