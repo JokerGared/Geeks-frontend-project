8bb31589f0d253a2cf56301c601bd0e57d237ed3
Rybalko Andrii <0andryha0@gmail.com>
2025-07-31 13:51:04 +0200

    change loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,24 @@
 import { ClipLoader } from 'react-spinners';
-import s from './Loader.module.css';
+import PropTypes from 'prop-types';
+import styles from './Loader.module.css';
 
-const Loader = ({ size = 24 }) => {
+const Loader = ({ size = 50, color = '#4fa94d', fullScreen = true }) => {
   return (
-    <div className={s.loader}>
-      <ClipLoader color="#1b831bbf" size={size} />
+    <div
+      className={fullScreen ? styles.overlay : styles.inline}
+      role="status"
+      aria-live="polite"
+      aria-label="Loading content..."
+    >
+      <ClipLoader size={size} color={color} aria-hidden="true" />
     </div>
   );
+};
+
+Loader.propTypes = {
+  size: PropTypes.number,
+  color: PropTypes.string,
+  fullScreen: PropTypes.bool,
 };
 
 export default Loader;
