f694ff9e619441cb934bdd0865d81b2e2d445e9a
Ira Efremova <ira.efremova95@gmail.com>
2025-07-31 03:05:35 +0300

    delete articlesPage code into by branch

--- src/redux/articles/operations.js
+++ src/redux/articles/operations.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 import { toast } from 'react-hot-toast';
+import axios from 'axios';
 
 export const fetchArticles = createAsyncThunk(
   'articles/fetchAll',
