2bfb3a44625c4e32c0b13f927235676dd03b880c
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-08-01 13:38:48 +0300

    popular articles section

--- src/components/PopularArticlesSection/PopularArticlesSection.jsx
+++ src/components/PopularArticlesSection/PopularArticlesSection.jsx
@@ -1,10 +1,42 @@
 import { Link } from 'react-router-dom';
+import ArticleItem from '../ArticleItem/ArticleItem';
+import clsx from 'clsx';
+import s from './PopularArticlesSection.module.css';
+import SectionTitle from '../SectionTitle/SectionTitle';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectPopularArticles } from '../../redux/articles/selectors';
+import { fetchArticles } from '../../redux/articles/operations';
+import { useEffect } from 'react';
 
 const PopularArticlesSection = () => {
+  const dispatch = useDispatch();
+  const articles = useSelector(selectPopularArticles);
+
+  useEffect(() => {
+    dispatch(fetchArticles());
+  }, [dispatch]);
+
   return (
-    <section id="popular-articles">
-      <h2>Popular Articles</h2>
-      <Link to="/articles">Go to all Articles</Link>
+    <section className={clsx('section')} id="popular-articles">
+      <div className={clsx(s.popularArticlesTitleLinkContainer)}>
+        <SectionTitle className={clsx(s.popularArticlesTitle)}>
+          Popular Articles
+        </SectionTitle>
+        <Link to="/articles" className={clsx(s.popularArticlesLink)}>
+          Go to all Articles
+          <svg width={24} height={24} className={clsx(s.topCreatorsIcon)}>
+            <use href="/icons.svg#icon-arrow"></use>
+          </svg>
+        </Link>
+      </div>
+
+      <ul className={clsx(s.popularArticlesList)}>
+        {articles.map((article) => (
+          <li key={article._id}>
+            <ArticleItem {...article} />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
--- src/redux/articles/selectors.js
+++ src/redux/articles/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectArticles = (state) => state.articles.items;
 
 export const selectCurrentArticle = (state) => state.articles.current;
@@ -10,3 +12,16 @@ export const selectArticlesHasNextPage = (state) => state.articles.hasNextPage;
 
 export const selectArticlesHasPreviousPage = (state) =>
   state.articles.hasPreviousPage;
+
+export const selectPopularArticles = createSelector(
+  [selectArticles],
+  (articles) => {
+    const popularArticles = articles
+      .toSorted((a, b) => b.rate - a.rate)
+      .slice();
+
+    const firstFourArticles = popularArticles.slice(0, 4);
+
+    return firstFourArticles;
+  },
+);
