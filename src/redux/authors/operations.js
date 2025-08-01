09f3951537ed269825387d3126365ad5411bed1c
Oleh <therkasoa@gmail.com>
2025-08-01 05:44:03 +0300

    small fix

--- src/redux/authors/operations.js
+++ src/redux/authors/operations.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import axios from '../../api/axiosInstance';
 import { toast } from 'react-hot-toast';
 
 export const fetchAuthors = createAsyncThunk(
