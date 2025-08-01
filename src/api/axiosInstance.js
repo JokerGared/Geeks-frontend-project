e9ec2e74bb491c14cda101e228a5813be3169a85
Oleh <therkasoa@gmail.com>
2025-08-01 05:58:36 +0300

    change dev axios behavior

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://geeks-backend-project.onrender.com',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
