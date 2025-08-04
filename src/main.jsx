63a420991a2413303940459272edceb9070657c4
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-08-04 17:48:49 +0300

    uncomment strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,13 @@ import { store } from './redux/store';
 import { persistor } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>,
-  // </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </StrictMode>,
 );
