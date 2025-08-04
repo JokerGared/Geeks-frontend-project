ef0ed74974e222a855c15195a6c55cee3d8c3826
Oleh <therkasoa@gmail.com>
2025-08-04 16:13:44 +0300

    made authors list universal

--- src/components/AuthorsList/AuthorsList.jsx
+++ src/components/AuthorsList/AuthorsList.jsx
@@ -1,56 +1,15 @@
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import AuthorsItem from '../AuthorsItem/AuthorsItem';
 import s from './AuthorsList.module.css';
-import Loader from '../Loader/Loader';
 
-import {
-  selectAuthors,
-  selectAuthorsLoading,
-  selectAuthorsError,
-  selectAuthorsHasNextPage,
-} from '../../redux/authors/selectors';
-
-import { fetchAuthors } from '../../redux/authors/operations';
-
-const AuthorsList = () => {
-  const dispatch = useDispatch();
-  const authors = useSelector(selectAuthors);
-  const isLoading = useSelector(selectAuthorsLoading);
-  const error = useSelector(selectAuthorsError);
-  const hasNextPage = useSelector(selectAuthorsHasNextPage);
-
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    dispatch(fetchAuthors({ page }));
-  }, [dispatch, page]);
-
-  const handleLoadMore = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
-
+const AuthorsList = ({ authors }) => {
   return (
-    <div>
-      {isLoading && <Loader />}
-      {error && (
-        <p className={s.status}>Sorry, this author could not be found.</p>
-      )}
-      {!isLoading && !error && (
-        <ul className={s.list}>
-          {authors.map((author) => (
-            <li key={author._id} className={s.item}>
-              <AuthorsItem {...author} />
-            </li>
-          ))}
-        </ul>
-      )}
-      {!isLoading && !error && hasNextPage && (
-        <button type="button" onClick={handleLoadMore} className={s.loadMore}>
-          Load more
-        </button>
-      )}
-    </div>
+    <ul className={s.list}>
+      {authors.map((author) => (
+        <li key={author._id} className={s.item}>
+          <AuthorsItem {...author} />
+        </li>
+      ))}
+    </ul>
   );
 };
 
--- src/pages/AuthorsPage/AuthorsPage.jsx
+++ src/pages/AuthorsPage/AuthorsPage.jsx
@@ -2,11 +2,50 @@ import s from './AuthorsPage.module.css';
 import AuthorsList from '../../components/AuthorsList/AuthorsList';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
 
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Loader from '../../components/Loader/Loader';
+
+import {
+  selectAuthors,
+  selectAuthorsLoading,
+  selectAuthorsError,
+  selectAuthorsHasNextPage,
+} from '../../redux/authors/selectors';
+
+import { fetchAuthors } from '../../redux/authors/operations';
+
 const AuthorsPage = () => {
+  const dispatch = useDispatch();
+  const authors = useSelector(selectAuthors);
+  const isLoading = useSelector(selectAuthorsLoading);
+  const error = useSelector(selectAuthorsError);
+  const hasNextPage = useSelector(selectAuthorsHasNextPage);
+
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    if (authors.length === 0) {
+      dispatch(fetchAuthors(page));
+    }
+  }, [dispatch, page]);
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
   return (
     <section className={s.authorsPage}>
       <SectionTitle className={s.title}>Authors</SectionTitle>
-      <AuthorsList />
+      {isLoading && <Loader />}
+      {error && (
+        <p className={s.status}>Sorry, this author could not be found.</p>
+      )}
+      {!isLoading && !error && <AuthorsList authors={authors} />}
+      {!isLoading && !error && hasNextPage && (
+        <button type="button" onClick={handleLoadMore} className={s.loadMore}>
+          Load more
+        </button>
+      )}
     </section>
   );
 };
