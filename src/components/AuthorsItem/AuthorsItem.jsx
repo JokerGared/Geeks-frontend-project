fd55be693c1e297db31d3a533c2e40be64d766cc
Oleh <therkasoa@gmail.com>
2025-08-07 13:12:43 +0300

    fix many bugs

--- src/components/AuthorsItem/AuthorsItem.jsx
+++ src/components/AuthorsItem/AuthorsItem.jsx
@@ -1,9 +1,11 @@
 import clsx from 'clsx';
 import s from './AuthorsItem.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const AuthorsItem = ({ _id, name, avatarUrl, avatarClassName = '' }) => {
   const authorName = name.split(' ')[0];
+  const location = useLocation();
+  const isInAuthors = location.pathname.startsWith('/authors');
   return (
     <Link to={`/authors/${_id}`} className={s.card}>
       {avatarUrl ? (
@@ -13,7 +15,9 @@ const AuthorsItem = ({ _id, name, avatarUrl, avatarClassName = '' }) => {
           className={clsx(s.avatar, avatarClassName)}
         />
       ) : (
-        <div className={s.fallbackAvatar}>
+        <div
+          className={isInAuthors ? s.fallbackAvatarAuthors : s.fallbackAvatar}
+        >
           {name
             .split(' ')
             .map((n) => n[0])
--- src/pages/AuthorsPage/AuthorsPage.jsx
+++ src/pages/AuthorsPage/AuthorsPage.jsx
@@ -37,7 +37,7 @@ const AuthorsPage = () => {
   return (
     <section className={s.authorsPage}>
       <SectionTitle className={s.title}>Authors</SectionTitle>
-      {error && authors.length === 0 ? (
+      {!isLoading && error && authors.length === 0 ? (
         <p className={s.status}>Sorry, this authors could not be found.</p>
       ) : (
         <AuthorsList authors={authors} />
